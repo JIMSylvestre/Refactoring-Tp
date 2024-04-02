0e14bdc49f3d45a5eab56da33668b16ac28583e9
JIMSylvestre <149988071+JIMSylvestre@users.noreply.github.com>
2024-04-02 20:38:14 +0300

    Add files via upload

---
+++ Homme.java
@@ -0,0 +1,18 @@
+package iut.bad;
+
+public class Homme extends Humain {
+	
+	public Homme() {
+		super();
+	}
+
+	public Homme(String nom, String prenom, int age) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+		this.age = age;
+	}
+	
+
+}
+
